284343457c6938133d42242d1fb1a03404a633e4
sachinkarn0003 <s.karn@aexonic.com>
2026-06-24 21:43:58 +0530

    Fix bootstrap logic in test_update_footer.php

--- public/test_update_footer.php
+++ public/test_update_footer.php
@@ -4,7 +4,8 @@ define('LARAVEL_START', microtime(true));
 require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$kernel->handle(Illuminate\Http\Request::create('/robots.txt', 'GET'));
 
 use App\Models\FooterSetting;
 
